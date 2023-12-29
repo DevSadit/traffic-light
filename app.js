85f41c25ef3e99c7c382f9fa27005d481a280fd7
Sadit <153073569+DevSadit@users.noreply.github.com>
2023-12-29 12:09:35 +0600

    traffic light

---
+++ app.js
@@ -0,0 +1,10 @@
+let color= `yellow`;
+if (color == `red`) {
+    console.log (`stop the bus and cars on the road`);
+}
+if (color == `yellow`) {
+    console.log (`drive the bus and cars slowly`);
+}
+if (color == `green`) {
+    console.log (`drive the bus and cars`);
+}
